1205c2e31e023c39ed06c92d1171fe0cb1048b87
Blair Taylor <blairntaylor@gmail.com>
2021-01-21 09:48:55 -0700

    Update psuedo code in the README

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 // Assignment Code
 
+// These are the password choices.
 var specChar = ["!", "@", "#", "$", "%", "&", "*"];
 var uppCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
 var lowCase = ["a", "b", "c", "d", "e", "f", "g". "h", "i", "j"];
 var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
+// HOW THE GENERATOR WORKS
 // click on the button
-// ask how many characters 8-128 collect information as a variable
-// do they want special characters collect information as a variable
-// do they want upper case collect information as a variable
+// Q1 User is prompted how many characters 8-128 
+// Collect this number as a variable
+// Q2 User is asked if they want special characters yes/no 
+// Collect information as a variable
+// Q3 User is asked if they want upper case yes/no
+// Collect this information as a variable
 // do they want lower case collect information as a variable
 
 // inside the generate password function
@@ -25,7 +30,7 @@ function = generatePassword ()
     var 
 
 
- if special === "true"
+ if special === "true";
 
 
  //make an empty array to push choices into.

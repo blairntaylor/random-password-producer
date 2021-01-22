1e78e7d6d472adf09941194e288e34273fddb03f
Blair Taylor <blairntaylor@gmail.com>
2021-01-22 09:39:05 -0700

    Committing code and pseudocode

--- script.js
+++ script.js
@@ -1,61 +1,83 @@
 // Assignment Code
 
-// These are the password choices.
+// These are the password options.
 var specChar = ["!", "@", "#", "$", "%", "&", "*"];
 var uppCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
 var lowCase = ["a", "b", "c", "d", "e", "f", "g". "h", "i", "j"];
-var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var numbs = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
-// HOW THE GENERATOR WORKS
-// click on the button
-// Q1 User is prompted how many characters 8-128 
-// Collect this number as a variable
-// Q2 User is asked if they want special characters yes/no 
-// Collect information as a variable
-// Q3 User is asked if they want upper case yes/no
-// Collect this information as a variable
-// do they want lower case collect information as a variable
-
-// inside the generate password function
-
-
-function = generatePassword ()
-
-      // ask if they want how many characters 8-128 
-      // ask do they want special characters collect information as a variable
-      // do they want upper case collect information as a variable
-      // do they want lower case collect information as a variable
-    var number = prompt("How many characters?") //collect var number
-    var special = confirm("Do you want special charactes?") //boolean true of false
-    var 
-
-
- if special === "true";
-
-
- //make an empty array to push choices into.
-
- finalCharacter.push(specChar)
-
-
- var finalCharacter = []
- var pass
-
-
-###########
-
-
-
+// CODE PROVIDED This code is to activate the code when the button is hit
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
+// CODE PROVIDED Starts the write password
+function writePassword () {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+///////////////
 
+  // Q1 User is prompted how many characters 8-128
+  // Collect this number as a variable 
+  var numOfChar = window.prompt("Enter a number between 8-128");
+  //alert if the user doesn't pick within 8-128
+  numOfChar = number(8 - 128)
+  alert
+
+  // Q2 User is asked if they want special characters - boolean 
+  // Collect information as a variable
+  var userSpecChar = window.confirm("Do you want to include special characters?");
+
+  // Q3 User is asked if they want upper case - boolean
+  // Collect this information as a variable
+  var userUppCase = window.confirm("Do you want to include upper case letters?");
+
+  // Q4 User is asked if they want lower case - boolean
+  // Collect this information as a variable
+  var userLowCase = window.confirm("Do you want to include lower case letters?");
+
+  // Q5 User is asked if they want numbers - boolean
+  // Collect this information as a variable
+  var userNums = window.confirm("Do you want to include numbers?");
+
+  // New password will be the number of characters the user  in Q1
+  // New password will concat the true conditions from the user selections
+
+  // Starts the generation of the password based on Q1-Q5 choices
+  function generatePassword ()
+    if (userSpecChar == true) {
+      // Random selection from 
+      var index = Math.floor(Math.random() * specChar.length);
+      var computerChoice = specChar[index];
+      // push a random selection into a new array
+
+    }
+
+    // CODE PROVIDED I think passwordText.value is variable of all input
   passwordText.value = password;
 
-}
+  }
+  // An array from the user selections
+ //make an empty array to push choices into.
+//  finalCharacter.push(specChar)
+//  var finalCharacter = []
+//  var pass
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+///////////////
+
+///////////////
+
+
+//The original provided JS code
+// var generateBtn = document.querySelector("#generate");
+
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   // I think passwordText.value is variable of all input
+//   passwordText.value = password;
+
+// }
+
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
